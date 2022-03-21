e1bc6498fb60f6434ce7d124dfe45cf56520406b
onionmccabbage <onionmccabbage@live.com>
2022-03-21 14:44:03 +0000

    Add files via upload

---
+++ 01_basic_vue/vue-project/src/components/Aero.vue
@@ -0,0 +1,29 @@
+<!-- NB all composed components MUST use 'setup' -->
+<script setup>
+import {ref, computed} from 'vue' 
+// immutable props are ALWAYS an object
+const props = defineProps({
+    product:Object
+})
+// inernal data models (refs and reactives)
+const thingy = ref('this is whatever I choose')
+// methods
+// function doStuff(){
+//     thingy.value = 'changed'
+// }
+// computed properties (anything we can derive we don't need to persist)
+const output = computed(()=>{ // this is the same as function output(){}
+    return `the field contains: ${thingy.value}`
+})
+</script>
+
+<template>
+    <li>{{product.id}} {{ product.n }}  {{ product.qty }}</li>
+    <!-- explore form fields -->
+    <!-- <input :value="thingy" @change="doStuff" /><p>field contains {{thingy}}</p> -->
+    <!-- v-model is TWO WAY BINDING - values go in, events come out -->
+    <input v-model="thingy" /><p>{{output}}</p>
+</template>
+
+<style>
+</style>
--- 01_basic_vue/vue-project/src/components/Greet.vue
+++ 01_basic_vue/vue-project/src/components/Greet.vue
@@ -1,5 +1,6 @@
 <script setup>
-import {ref} from 'vue' // ref lets us create references to values scoped to this component
+import {ref, reactive} from 'vue'
+import Aero from './Aero.vue'; // ref lets us create references to values scoped to this component
 defineProps({ // props are immutable
   msg: { // these are ES objects {name:value}
     type: String,
@@ -19,12 +20,30 @@ defineProps({ // props are immutable
 const now = ref(new Date())
 const products = ref( // ref can take ANY data type
   [
-    {id:0, n:'aileron', qty:2},
+    {id:0, n:'aileron', qty:2}, // always provide an ID for v-for keys
     {id:1, n:'flap', qty:4},
     {id:2, n:'empennage', qty:1}
   ]
 )
 const show = ref(true) // a reference to a boolean value
+// some reactive values nb reactive MUST be a collection (e.g. an object, an array or a class)
+const state    = reactive({count:0})
+const obj      = reactive({
+  nested:{count:0},
+  arr   :['alpha', 'beta']
+})
+// ref can be ANY data type
+const refCount = ref(0) // NB all refs will expose a .value for us to use
+// here are some methods for this component
+function increment(){
+  state.count++
+  refCount.value++ // NB ref ALWAYS refers to a 'value' in script
+}
+function mutateDeeply(){
+  // mutate deep members of the reactive object called 'obj'
+  obj.nested.count++
+  obj.arr.push('gamma')
+}
 </script>
 <template>
   <article>
@@ -38,15 +57,30 @@ const show = ref(true) // a reference to a boolean value
       <img v-if="show" :src='imglocation' title="a kitten" />
       <br/>
       <!-- toggle show between true and false using a click event handler -->
+      <!-- @ is shorthand for v-on: NB in html we DO NOT say .value for refs-->
       <button @click="show=!show"  >Toggle</button>
     </section>
     <section>
       <!-- here we will iterate over the collection of product items -->
       <ul>
-        <li v-for="product of products">
+        <!-- v-for of is for arrays and in is for objects -->
+        <!-- <li v-for="product of products">
           Item: {{product.n}} quantity: {{product.qty}}
-        </li>
+        </li> -->
+        <!-- we can use our Aero component to iterate over -->
+        <!-- NB for does NOT unpack the members of the iterable -->
+        <Aero v-for="item in products" :product='item' :key='item.id'/>
       </ul>
+    </section>
+    <section>
+      <!-- // buttons using ref and reactive model values -->
+      <button @click="increment" ref="btn">{{state.count}} and {{refCount}}</button>
+      <!-- the .once makes the button respond only once -->
+      <button @click.once="mutateDeeply">{{obj.nested.count}}</button>
+      <br/>
+      <ol>
+        <li v-for="item in obj.arr" >{{item}}</li>
+      </ol>
     </section>
   </article>
 </template>
